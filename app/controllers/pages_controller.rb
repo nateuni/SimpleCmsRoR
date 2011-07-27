e82ec7adfd857f9d6d4c9ebf9a8ec47cfbc71528
nateuni <nathansturgess.uni@gmail.com>
2011-07-27 18:51:37 +1000

    Adding CRUD for Subject and Page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,55 @@
 class PagesController < ApplicationController
   layout 'admin'
+  
+  def index
+    list
+    render('list')
+  end
+  
+  def new
+     @page = Page.new(:name => "default")
+  end
+  
+  def create
+    @page = Page.new()
+    if @page.save
+      flash[:notice] = "Page created"
+      redirect_to(:action => 'list')
+    else
+      render('new')
+    end    
+  end
+  
+  def list
+    @pages = Page.order("pages.position ASC")
+  end
+  
+  def show
+    @page = Page.find(params[:id])
+  end
+  
+  def edit
+    @page = Page.find(params[:id])
+  end
+  
+  def update
+   @page = Page.find(params[:id])
+   if @page.update_attributes(params[:page])
+     flash[:notice] = "Page updated."
+     redirect_to(:action => 'show', :id => @page.id)
+   else
+     render('edit')
+   end
+  end
+  
+  def delete
+    @page = Page.find(params[:id])
+  end
+  
+  def destroy
+    Page.find(params[:id]).destroy
+    flash[:notice] = "Page Destroyed"
+    redirect_to(:action => 'list')
+  end      
+              
 end
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,3 +1,54 @@
 class SectionsController < ApplicationController
   layout 'admin'
+  
+  def index
+    list
+    render('list')
+  end
+  
+  def new
+     @section = Section.new(:name => "default")
+  end
+  
+  def create
+    @page = @Page.new
+    if @page.save
+      flash[:notice] = "Section created"
+      redirect_to(:action => 'list')
+    else
+      render('new')  
+  end
+  
+  def list
+    @sections = Section.order("sections.position ASC")
+  end
+  
+  def show
+    @section = Section.find(params[:id])
+  end
+  
+  def edit
+    @section = Section.find(params[:id])
+  end
+  
+  def update
+    @section = Section.find(params[:id])
+    if @section.save
+      flash[:notice] = "Section updated"
+      redirect_to(:action => 'show' :id => @section.id)
+    else
+      render('edit')
+    end  
+  end
+  
+  def delete
+    @section = Section.find(params[:id])
+  end
+  
+  def destroy
+    Section.find(params[:id]).destroy
+    flash[:notice] = "Section destroyed"
+    redirect_to(:action => 'list')
+  end
+  
 end
